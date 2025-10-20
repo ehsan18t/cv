e3c61092640580a35efb040828829c8feb453c27
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2025-10-20 17:42:23 +0600

    updated build mechanism

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -18,26 +18,48 @@ if [[ $# -gt 0 ]]; then
   run_external_command "$@"
 fi
 
-TARGET=${TARGET:-fullstack.tex}
 OUTDIR=${OUTDIR:-pdf}
 AUXDIR=${AUXDIR:-build}
 EXTRA_ARGS=${EXTRA_ARGS:-}
+TARGETS=${TARGET:-}
 
 mkdir -p "${OUTDIR}" "${AUXDIR}"
 
 IFS=' ' read -r -a extra_array <<< "${EXTRA_ARGS}"
 
-latexmk \
-  -synctex=0 \
-  -interaction=nonstopmode \
-  -file-line-error \
-  -lualatex \
-  -shell-escape \
-  -outdir="${OUTDIR}" \
-  -auxdir="${AUXDIR}" \
-  "${extra_array[@]}" \
-  "${TARGET}"
-status=$?
+declare -a targets=()
+
+if [[ -n "${TARGETS}" ]]; then
+  read -r -a targets <<< "${TARGETS}"
+else
+  while IFS= read -r -d '' file; do
+    file=${file#./}
+    targets+=("${file}")
+  done < <(find . -maxdepth 1 -type f -name '*.tex' -print0)
+fi
+
+if [[ ${#targets[@]} -eq 0 ]]; then
+  echo "entrypoint: no root-level .tex files found; nothing to build."
+  exit 0
+fi
+
+status=0
+for target in "${targets[@]}"; do
+  echo "entrypoint: building ${target}"
+  if ! latexmk \
+    -synctex=0 \
+    -interaction=nonstopmode \
+    -file-line-error \
+    -lualatex \
+    -shell-escape \
+    -outdir="${OUTDIR}" \
+    -auxdir="${AUXDIR}" \
+    "${extra_array[@]}" \
+    "${target}"; then
+    status=$?
+    break
+  fi
+done
 
 cleanup
 
